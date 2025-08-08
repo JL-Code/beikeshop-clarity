aa824f59082c7c31028f3d5f9aa89f7350caccc4
JL-Code <jiangliu0316@outlook.com>
2025-08-08 09:57:40 +0800

    refactor: update Clarity plugin script handling and add logging

--- plugins/Clarity/Bootstrap.php
+++ plugins/Clarity/Bootstrap.php
@@ -2,8 +2,7 @@
 
 namespace Plugin\Clarity;
 
-use Beike\Admin\Http\Resources\PluginResource;
-use Beike\Plugin\Plugin;
+use Illuminate\Support\Facades\Log;
 
 class Bootstrap
 {
@@ -17,26 +16,16 @@ class Bootstrap
 
     public function addScript(): void
     {
-//        $pluginResource = (new PluginResource($plugin))->jsonSerialize();
-//        $version = $plugin->getVersion();
         add_hook_blade('layout.header.code', function ($callback, $output, $data) {
             $script = plugin_setting('clarity.value') ?? '';
 
-            // $script = <<<EOF
-            // <script type="text/javascript">
-            //     (function(c,l,a,r,i,t,y){
-            //         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            //         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            //         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            //     })(window, document, "clarity", "script", "m12gb2u3dy");
-            // </script>
-            // EOF;
-            $logs = <<<EOF
-             <script type="text/javascript">
-                 console.log(`clarity plugin enabled`)
-            </script>
-            EOF;
-            return $output . $script . $logs;
+            Log::info("clarity plugin value: " . $script);
+
+            // return $output . $script . $logs;
+
+            $data['script'] = $script;
+
+            return view('Clarity::shop.script_container', $data)->render();
         });
     }
 }
